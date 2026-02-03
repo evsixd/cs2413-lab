5f26b4b00a80d12b4bec96cabdc3b55557cbc5ec
evsixd <evsingle@ttu.edu>
2026-02-03 09:49:46 -0600

    Actual final.

--- lab01/src/three_sum_sorted.c
+++ lab01/src/three_sum_sorted.c
@@ -2,45 +2,18 @@
 
 int three_sum_zero_sorted_one(const int* nums, int n, int* out_i, int* out_j, int* out_k) {
     
-   int i = 0;
-   int j = 0;
-   int k = 0;
-
-   if(nums[i] + nums[j] + nums[k] != 0) {
-        if(nums[i] + nums[j] + nums[k] < 0) {
-            while(nums[i] + nums[j] + nums[k] < 0) {
-                i++;
-            }
-            if(nums[i] + nums[j] + nums[k] == 0) {
-                *out_i = i;
-                *out_j = j;
-                *out_k = k;
-                return 1;
-            } else {
-                while(nums[i] + nums[j] + nums[k] < 0) {
-                    j++;
-                }
-                if(nums[i] + nums[j] + nums[k] == 0) {
+    for(int i = 1; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            for(int k = j; k < n; k++) {
+                if(nums[i] + nums[j] + nums[k] == 0 && i < j && j < k) {
                     *out_i = i;
                     *out_j = j;
                     *out_k = k;
                     return 1;
-                } else {
-                    while(nums[i] + nums[j] + nums[k] < 0) {
-                        k++;
-                    }
-                    if(nums[i] + nums[j] + nums[k] == 0) {
-                        *out_i = i;
-                        *out_j = j;
-                        *out_k = k;
-                        return 1;
-                    } else {
-                        return 0;
-                    }
                 }
             }
         }
-   }
+    }
 
     return 0;
 }
