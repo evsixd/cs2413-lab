87f3360578479b9149803a56b8df5c3cadc5c33e
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 <evsingle@ttu.edu>
2026-02-24 14:22:20 -0600

    lab03 Question 4

--- lab03/Question4/student.c
+++ lab03/Question4/student.c
@@ -14,5 +14,18 @@
 
 int middleNodeValue(struct ListNode *head) {
     // TODO: implement
+    struct ListNode* follower = head;
 
+    int counter = 0;
+    for(struct ListNode* leader = head->next; leader != NULL; leader = leader->next) {
+        counter++;
+        if(counter == 2) {
+            follower = follower->next;
+            counter = 0;
+        }
+    }
+    if(counter == 1) {
+        return follower->next->val;
+    }
+    return follower->val;
 }
