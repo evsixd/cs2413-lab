6af1e5627a93ab66d6e5b4f7f1e7ed2e6f66c6a3
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 <evsingle@ttu.edu>
2026-02-24 13:41:05 -0600

    lab03 Question 2.

--- lab03/Question2/student.c
+++ lab03/Question2/student.c
@@ -13,5 +13,38 @@
 #include "student.h"
 
 struct ListNode* swapPairs(struct ListNode* head) {
-      // TODO: implement
+
+      struct ListNode* one = NULL;
+      struct ListNode* returning = NULL;
+      struct ListNode* prior = NULL;
+
+      for(struct ListNode* leader = head; leader != NULL; leader = leader->next) {
+            if(one == NULL) {
+                  one = leader;
+                  continue;
+            } else {
+                  one->next = leader->next;
+
+                  if(returning == NULL) {
+                        returning = leader;
+                  }
+
+                  if(prior == NULL) {
+                        prior = one;
+                  } else {
+                        prior->next = leader;
+                        prior = one;
+                  }
+
+                  leader->next = one;
+                  leader = one;
+                  one = NULL;
+            }
+
+      }
+
+      if(returning == NULL) {
+            return head;
+      }
+      return returning;
 }
