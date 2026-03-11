0ca62eaeb3c9198aa46bb5dba29ef4c121874b55
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 <evsingle@ttu.edu>
2026-03-11 11:25:16 -0500

    lab04 Done.

--- lab04/Question3/student.c
+++ lab04/Question3/student.c
@@ -1,4 +1,6 @@
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /*
 Question 3: Sum Root to Leaf Numbers
@@ -55,7 +57,112 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
+struct NodeTracker {
+  int* summing; 
+  int sizing; // starts at 1, not 0 (overshoots summing)
+  struct TreeNode *assignment;
+};
+
+struct NodeTracker* newTracker(int insert, struct TreeNode *assign) {
+  struct NodeTracker* returning = malloc(sizeof(struct NodeTracker));
+  returning->sizing= 1;
+  returning->summing = malloc(sizeof(int));
+  returning->summing[0] = insert;
+  returning->assignment = assign;
+
+  return returning;
+}
+
+void trackerFwd(struct NodeTracker* tracker, int direction) { // if direction == 1, go left, if == --1, go right
+  tracker->sizing = tracker->sizing + 1;
+  tracker->summing = realloc(tracker->summing, sizeof(int) * tracker->sizing);
+  if(direction == 1) { // left
+    tracker->summing[tracker->sizing - 1] = tracker->assignment->left->val;
+    tracker->assignment = tracker->assignment->left;
+  }
+  if(direction == -1) { // right
+    tracker->summing[tracker->sizing - 1] = tracker->assignment->right->val;
+    tracker->assignment = tracker->assignment->right;
+  }
+}
+
+struct NodeTracker* cloneTracker(struct NodeTracker* tracker) {
+  struct NodeTracker* returning = malloc(sizeof(struct NodeTracker));
+
+  returning->summing = malloc(sizeof(int) * tracker->sizing);
+  for(int i = 0; i < tracker->sizing; i++) {
+    returning->summing[i] = tracker->summing[i];
+  }
+
+  returning->sizing = tracker->sizing;
+  returning->assignment = tracker->assignment;
+
+  return returning;
+}
+
+// actual function start
 
 int sumNumbers(struct TreeNode* root) {
-      // TODO: implement
+  if(root == NULL) {
+    return 0;
+  }
+
+  struct NodeTracker** nodify = malloc(sizeof(struct NodeTracker*));
+  nodify[0] = newTracker(root->val, root);
+
+  int nodify_s = 1;
+
+  while(1) {
+    int nulls = 1;
+
+    for(int i = nodify_s - 1; i >= 0; i--) { // iterate top to bottom through list of tracker nodes
+
+      if(nodify[i]->assignment->right != NULL && nodify[i]->assignment->left != NULL) {
+        struct NodeTracker* clone_r = cloneTracker(nodify[i]); // must clone to extend list
+        nulls = 0;
+
+        nodify_s++;
+        nodify = realloc(nodify, sizeof(struct NodeTracker*) * nodify_s);
+        for(int j = nodify_s - 1; j > i + 1; j--) {
+          nodify[j] = nodify[j - 1];
+        }
+        nodify[i + 1] = clone_r; // if 2 paths are available, must make room for new path and clone to new path
+
+
+        trackerFwd(nodify[i + 1], -1);
+        trackerFwd(nodify[i], 1); // forward lower node left, upper node right
+
+      } else if(nodify[i]->assignment->left != NULL) {
+        nulls = 0;
+
+
+        trackerFwd(nodify[i], 1); // if only left has path, forward left
+      } else if(nodify[i]->assignment->right != NULL) {
+        nulls = 0;
+
+        trackerFwd(nodify[i], -1); // if only right has path, forward right
+      }
+
+    }
+
+    if(nulls == 1) {
+      break; // terminate loop once all node trackers have reached endpoints
+    }
+  }
+
+  // now that tracker list is fully compiled, go through and complete each part of the sum
+  int sum = 0;
+  for(int i = 0; i < nodify_s; i++) {
+    int mult = 1;
+    for(int j = nodify[i]->sizing - 1; j >= 0; j--) {
+      sum += nodify[i]->summing[j] * mult; // add each digit to the sum based on its position relative to depth
+      mult *= 10;
+    }
+    free(nodify[i]);
+  }
+
+  free(nodify);
+
+  return sum;
+
 }
